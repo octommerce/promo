9c129038503eb123e72ca05ba3c922da3b5411c8
Trias Nur Rahman <contact@triasrahman.com>
2016-09-15 18:00:01 +0700

    Listen on create order, add the discount

--- Plugin.php
+++ Plugin.php
@@ -1,7 +1,10 @@
 <?php namespace Octommerce\Promo;
 
-use Octommerce\Promo\Classes\PromoManager;
+use Event;
 use System\Classes\PluginBase;
+use Octommerce\Promo\Models\Coupon;
+use Octommerce\Promo\Classes\PromoManager;
+use Octommerce\Promo\Classes\Validator;
 
 class Plugin extends PluginBase
 {
@@ -37,6 +40,44 @@ class Plugin extends PluginBase
 
             $promoManager->addRuleFields($form);
 
+        });
+
+        Event::listen('order.afterCreate', function($order, $data) {
+            $code = isset($data['code']) ? trim($data['code']) : '';
+
+            if (!$code)
+                return;
+
+            $validator = Validator::instance();
+
+            $options = [
+                'products' => $order->products,
+            ];
+
+            $target = [
+                'subtotal' => $order->subtotal,
+            ];
+
+            $count = 1;
+
+            // Validate
+            if($validator->validate($code, $options, $target, $count)) {
+
+                // Get the coupon
+                $coupon = Coupon::find($validator->output['coupon_id']);
+
+                // Hold the coupon based on determined amount
+                $redemption = $coupon->hold($order->user_id, $count);
+
+                // $this->order->coupon_code = $coupon->code;
+                // $this->order->coupon_redemption_id = $redemption->id;
+
+                if (isset($validator->output['target']['subtotal'])) {
+                    $order->discount = $validator->output['target']['subtotal'];
+                    $order->save();
+                }
+
+            }
         });
     }
 }
